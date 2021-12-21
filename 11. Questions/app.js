7aef879dda512df912939842967ccade07034cc6
Riola <riolalama@live.com>
2021-12-21 17:17:54 +0100

    Changed styles

--- 11. Questions/app.js
+++ 11. Questions/app.js
@@ -15,17 +15,3 @@ questions.forEach(question => {
 });
 
 
-
-
-
-
-
-// const btns = document.querySelectorAll('.question-btn');
-
-// btns.forEach(btn => {
-// btn.addEventListener('click', e => {
-// //    console.log(e.currentTarget.parentElement.parentElement); 
-// const question = e.currentTarget.parentElement.parentElement
-// question.classList.toggle('show-text');
-// })
-// });
