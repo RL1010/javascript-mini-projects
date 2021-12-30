9d31aab4d080e3301010314303c00f6480cc8206
Riola <riolalama@live.com>
2021-12-30 16:11:54 +0100

    Final version

--- 13. Form Validator/script.js
+++ 13. Form Validator/script.js
@@ -4,64 +4,62 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
 
-function showError (input, message){
-    const formControl = input.parentElement;
-    formControl.classList.add('error')
-    const small = formControl.querySelector('small')
-    small.textContent = message;
-  
-}
-function showSuccess(input){
-    const formControl = input.parentElement;
-    formControl.classList.add('success')
-}
-
-function checkEmail (input){
-    const re =   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if(re.test(input.value.trim())){
-        showSuccess(input)
-    }else{
-        showError(input,'Email is not valid')
-    }
-}
-
-function checkRequired(inputArray){
-    inputArray.forEach(input => {
-        if(input.value.trim() === ''){
-            showError(input, `${getFieldName(input)} is required`)
-        }else{
-            showSuccess(input)
-        }
-
-    })
-}
-
-    function checkLength(input, min, max){
-        if(input.value.length < min){
-            showError(input, `${getFieldName(input)} must be at least ${min} characters`)
-        }else if ( input.value.length > max){
-            showError(input, `${getFieldName(input)} must be less than ${max} characters`)
-        }else{
-            showSuccess(input)
-        }
-    }
-    function checkPasswordsMatch(input1, input2){
-        if(input1.value !== input2.value){
-            showError(input2, 'Passwords do not match')
-        }
-    }
-
-    function getFieldName (input){
-        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-    }
-
-
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    checkRequired([username,email, password, password2])
-    checkLength(username, 3, 15)
-    checkLength(password, 6, 25)
-    checkEmail(email)
-    checkPasswordsMatch(password,password2)
+	e.preventDefault();
+	
+	checkInputs();
+});
 
-})
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!checkEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+   	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+
+function checkEmail (email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
