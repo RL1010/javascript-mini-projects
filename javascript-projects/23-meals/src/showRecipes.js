6dab8b8ec1f4dc6d7081d6e24cea88a3094236b5
Riola <riolalama@live.com>
2022-01-10 17:44:58 +0100

    v2

--- javascript-projects/23-meals/src/showRecipes.js
+++ javascript-projects/23-meals/src/showRecipes.js
@@ -2,17 +2,11 @@ import fetchRecipes from './fetchRecipes.js';
 import displayRecipes from './displayRecipes.js';
 import setRecipe from './setRecipe.js';
 const showRecipe = async (url) => {
-<<<<<<< HEAD
+
   // fetch meals
   const data = await fetchRecipes(url);
-
-  // display meals
-=======
-  // fetch recipes
-  const data = await fetchRecipes(url);
-
+  
   // display recipes
->>>>>>> c785c2c41e06fc97b59ab20384bf5002c46066ad
   const section = await displayRecipes(data);
   if (section) {
     setRecipe(section);
